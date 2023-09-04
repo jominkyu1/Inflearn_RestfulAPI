a49eb73992ae8c78ae3f9da09b6212e40b0af00a
jominkyu1 <jominkyu@gmail.com>
2023-09-05 00:52:43 +0900

    0905 SpringBoot Maven 프로젝트 생성

---
+++ src/main/java/com/example/restfulapiservice/RestfulApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulApiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfulapiservice/RestfulApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
